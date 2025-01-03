404069c2820a25029d30cb06062363b262e8b186
matthewabates <matthew.a.bates@gmail.com>
2025-01-02 23:04:15 -0600

    calculator works

--- script.js
+++ script.js
@@ -1,58 +1,57 @@
-var x;
-var y;
+var memory;
 var operator;
+var startNewNumber = true;
 const display = document.querySelector("#display");
 
-document.querySelectorAll('.number').forEach(button => {
+document.querySelectorAll('button').forEach(button => {
     button.addEventListener('click', event => {
-        display.value += event.target.id
+        if (button.classList.contains('operand')) {
+            let number = event.target.id
+            if (startNewNumber) {
+                console.log('new number started')
+                display.value = undefined
+                startNewNumber = false
+            }
+            display.value += number
+        } else if (button.classList.contains('operator')) {
+            //if we have't calculated the previous operation yet, do that first!
+            if (operator != undefined && display.value != undefined && memory != undefined) {
+                calculate()
+            } 
+            operator = event.target.id
+            memory = display.value
+            startNewNumber = true; 
+        } else if (button.id === 'clear') {
+            display.value = "undefined"
+            memory = undefined 
+            operator = undefined
+        } else if (button.id === 'equal') {
+            if (operator) { calculate() }
+        } else if (button.id === 'negate') {
+            display.value = -display.value
+        } else if (button.id === 'percent') {
+            display.value = display.value / 100
+        } else if (button.id === 'backspace') {
+            display.value = Math.floor(display.value / 10)
+        }
     })
 })
 
-document.querySelectorAll('.operator').forEach(button => {
-    button.addEventListener('click', event => {
-        operator = event.target.id
-        x = display.value
-        display.value = ""
-    })
-})
-
-document.querySelectorAll('#equal').forEach(button => {
-    button.addEventListener('click', event => {
-        y = display.value;
-        display.value = operate(operator, x, y)
-    })
-})
-
-document.querySelectorAll('#clear').forEach(button => {
-    button.addEventListener('click', event => {
-        display.value = undefined
-        x = undefined
-        y = undefined              
-    })
-})
- 
-function operate(operator, x, y) {
-    switch (operator) {
-        case "+": return add(x, y)
-        case "-": return subtract(x, y)
-        case "*": return multiply(x, y)
-        case "/": return divide(x, y)            
-    }
+function calculate() {
+    memory = operate(memory, operator, display.value)
+    operator = undefined
+    display.value = memory
+    startNewNumber = true;
 }
-
-function add(x, y) {
-	return Number(x) + Number(y)
-};
-
-function subtract(x, y) {
-	return Number(x) - Number(y)
-};
-
-function multiply(x, y) {
-    return Number(x) * Number(y)
-};
-
-function divide(x, y) {
-    return Number(x) / Number(y)
+ 
+function operate(x, operator, y) {
+    console.log (x, operator, y)
+    x = Number(x)
+    y = Number(y)
+    switch (operator) {
+        case "+": return x + y
+        case "-": return x - y
+        case "*": return x * y
+        case "/": return x / y
+    }
 }
